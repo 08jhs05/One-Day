828c4273cc0cd2fc264869c693a03f0c4b31bf28
Jay Shin <08jhs05@gmail.com>
2020-01-03 16:44:16 -0500

    Initial Commit

---
+++ One DayTests/One_DayTests.swift
@@ -0,0 +1,34 @@
+//
+//  One_DayTests.swift
+//  One DayTests
+//
+//  Created by Jae Ho Shin on 2020-01-03.
+//  Copyright © 2020 Jae Ho Shin. All rights reserved.
+//
+
+import XCTest
+@testable import One_Day
+
+class One_DayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
